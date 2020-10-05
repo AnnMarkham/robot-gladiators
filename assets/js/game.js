1710521058b7f6177f2b6ee9b51bcf8f3ba88e74
AnnMarkham <ann.e.markham@outlook.com>
2020-10-05 12:08:42 -0700

    created and executed functions; interactd with the user and store data

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,21 @@
 
-window.alert("this is an alert!JavaScript is running!);")
+// this creates a function named "fight"
+var playerName = window.prompt("what is your robot's name?");
+
+//what is theis?
+console.log(playerName);
+
+console.log("Thislogs a string, good for leaving yourself a message");
+
+//this will do math and log 20
+console.log(10 + 10);
+
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+  window.alert("The fight has begun!")
+}
+
+
+//fight();
